6bbf9790f0493e7eaacd19f3f51e9feebd0f6295
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-08-11 18:02:32 +0900

    first commit input_dir

---
+++ input/includes/input.h
@@ -0,0 +1,11 @@
+#ifndef INPUT_H
+# define INPUT_H
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+
+#endif
