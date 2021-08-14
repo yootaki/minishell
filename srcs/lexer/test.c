16409ebb1b2b032894b37b41677361fa627cbccd
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-08-14 20:11:50 +0900

    テスト用関数

---
+++ srcs/lexer/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	//char	*c = '.';
+
+	printf("c = %d\n", 'a');
+	printf("c = %d\n", '0');
+	printf("c = %d\n", 'A');
+	printf("c = %d\n", '.');
+	return (0);
+}
