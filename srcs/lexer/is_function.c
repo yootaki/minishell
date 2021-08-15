e9ffac23b586b6379e8d8d9e326fec2981389d4c
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-08-16 03:14:41 +0900

    main()からis_alnum(),isdigit(),isquort(),is_else()をis_function()に移動

---
+++ srcs/lexer/is_function.c
@@ -0,0 +1,70 @@
+#include "input.h"
+
+void	is_alnum(char **cmd, size_t *char_cnt)
+{
+	while (ft_isalnum(**cmd) && **cmd != '\0')
+	{
+		(*cmd)++;
+		(*char_cnt)++;
+	}
+}
+
+void	is_digit(char **cmd, size_t *char_cnt)
+{
+	while ((ft_isdigit(**cmd) || **cmd == '.') && **cmd != '\0')
+	{
+		(*cmd)++;
+		(*char_cnt)++;
+	}
+}
+
+void	is_quort(char **cmd, size_t *char_cnt)
+{
+	char	ch;
+
+	ch = **cmd;
+	(*cmd)++;
+	(*char_cnt)++;
+	while (**cmd != ch && **cmd != '\0')
+	{
+		(*cmd)++;
+		(*char_cnt)++;
+	}
+	(*cmd)++;
+	(*char_cnt)++;
+	if (**cmd == CHAR_QOUTE || **cmd == CHAR_DQUOTE)
+	{
+		ch = **cmd;
+		(*cmd)++;
+		(*char_cnt)++;
+		while (**cmd != ch && **cmd != '\0')
+		{
+			(*cmd)++;
+			(*char_cnt)++;
+		}
+		(*cmd)++;
+		(*char_cnt)++;
+	}
+}
+
+void	is_else(char **cmd, size_t *char_cnt)
+{
+	char	ch;
+
+	if (**cmd == CHAR_GREATER || **cmd == CHAR_LESSER)
+	{
+		ch = **cmd;
+		(*cmd)++;
+		(*char_cnt)++;
+		if (**cmd == ch)
+		{
+			(*cmd)++;
+			(*char_cnt)++;
+		}
+	}
+	else
+	{
+		(*cmd)++;
+		(*char_cnt)++;
+	}
+}
