7d598ba2e1400c6f4bce709d9a198b09fb0f7939
Yohei Otaki <76095760+popeye-yupi@users.noreply.github.com>
2022-01-17 15:26:17 +0900

    コメントを追加　

--- srcs/utils/signal_main.c
+++ srcs/utils/signal_main.c
@@ -6,19 +6,22 @@
 /*   By: yootaki <yootaki@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 14:03:27 by hryuuta           #+#    #+#             */
-/*   Updated: 2022/01/17 15:20:40 by yootaki          ###   ########.fr       */
+/*   Updated: 2022/01/17 15:25:58 by yootaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
+/* ctrl + C */
 void	sig_int_input()
 {
-	// ft_putstr_fd("\b\b  \b\n", STDERR_FILENO);
+	// \bは1文字分カーソルを左にずらしている
+	// MINISHELLはutils.hで定義している。（"minishell >> "）
 	ft_putstr_fd("\b\b  \b\n", STDERR_FILENO);
 	ft_putstr_fd(MINISHELL, STDERR_FILENO);
 }
 
+/* ctrl + \ */
 void	sig_quit_input()
 {
 	ft_putstr_fd("\b\b  \b\b", STDERR_FILENO);
